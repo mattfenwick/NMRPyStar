dc8bcb2b54512e3a746a3a3e3a49e0f6fa7ba36e
Matt Fenwick <mfenwick100@gmail.com>
2013-04-08 16:52:57 -0400

    nmrstar test file and test driver script including profiling

---
+++ src/nmrstar/main.py
@@ -0,0 +1,21 @@
+import nmrstar.tokenizer as t
+import parse.position as p
+import parse.conslist as c
+import cProfile
+import pstats
+
+
+def parseFile(input):
+    return t.scanner.parse(c.ConsList.fromIterable(p.addLineCol(input)), None)
+    
+    
+def readMe():
+    with open('test/nmrstar/bmrb17661.txt', 'r') as f:
+        return f.read()
+        
+        
+def profile():
+    # can't use the following b/c parseFile and readMe need to be in scope:
+    #   cProfile.run('parseFile(readMe())', 'profile.txt')
+    cProfile.runctx('parseFile(readMe())', {}, {'parseFile': parseFile, 'readMe': readMe}, 'profile.txt')
+    return pstats.Stats('profile.txt')
