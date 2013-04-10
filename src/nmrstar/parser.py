eb495671acfde6ce803b23c502042fb321c4e5ae
Matt Fenwick <mfenwick100@gmail.com>
2013-04-10 16:30:06 -0400

    module for tokenizing and ast-parsing star files

---
+++ src/nmrstar/parser.py
@@ -0,0 +1,25 @@
+import nmrstar.tokenizer as t
+import parse.position as p
+import parse.conslist as c
+import nmrstar.astparser as a
+
+
+
+def tokenize(str):
+    lcChars = c.ConsList.fromIterable(p.addLineCol(str))
+    return t.scanner.parse(lcChars, None)
+    
+
+JUNKTYPES = set(['comment', 'whitespace', 'newlines'])
+    
+def stripJunk(tokens):
+    return [t for t in tokens if t.tokentype not in JUNKTYPES]
+
+
+def parse(input):
+    tokens = tokenize(input)
+    def astParse(tokenization):
+        stripped = stripJunk(tokenization['result'])
+        return a.nmrstar.parse(c.ConsList.fromIterable(stripped), None)
+    return tokens.bind(astParse)
+    
