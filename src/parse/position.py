26126222554565fb5d88054a80994f317d401217
Matt Fenwick <mfenwick100@gmail.com>
2013-04-22 16:33:21 -0400

    PEP8 style cleanup:  get rid of extraneous whitespace

--- src/parse/position.py
+++ src/parse/position.py
@@ -5,17 +5,17 @@ class Char(object):
     def __init__(self, char, meta):
         self.char = char
         self.meta = meta
-        
+
     def __repr__(self):
         return repr(self.__dict__)
-    
+
     def __eq__(self, other):
         return self.__dict__ == other.__dict__
-        
 
-def addLineCol(input):
+
+def addLineCol(string):
     line, column, chars = 1, 1, []
-    for x in input:
+    for x in string:
         chars.append(Char(x, {'line': line, 'column': column}))
         if x == '\n':
             line += 1
