e5ec8a9b694a7848ea0ac6497883c9062060d496
Matt Fenwick <mfenwick100@gmail.com>
2013-05-10 16:04:49 -0400

    ConsList:  add docstrings to methods, and new method getRest for retrieving the remaining elements as an unwrapped (i.e. primitive) collection

--- src/parse/conslist.py
+++ src/parse/conslist.py
@@ -1,6 +1,11 @@
 
 
 class ConsList(object):
+    '''
+    A data structure that supports constant-time first/rest slicing.
+    The input sequence is never copied or modified -- all the slicing
+    has to do is increment a position counter and create a new wrapper.
+    '''
 
     def __init__(self, seq, start):
         self.seq = seq
@@ -10,14 +15,27 @@ class ConsList(object):
         return self.start >= len(self.seq)
         
     def first(self):
+        '''
+        Returns first element.  Throws exception if empty.
+        '''
         if not self.isEmpty():
             return self.seq[self.start]
         raise ValueError('cannot get first element of empty sequence')
         
     def rest(self):
+        '''
+        Return ConsList of all but the first element.
+        Throws exception if ConsList is already empty.
+        '''
         if not self.isEmpty():
             return ConsList(self.seq, self.start + 1)
         raise ValueError('cannot get rest of empty sequence')
+    
+    def getRest(self):
+        '''
+        Return primitype sequence type of remaining elements.
+        '''
+        return self.seq[self.start:]
     
     @staticmethod
     def fromIterable(seq):
@@ -30,5 +48,5 @@ class ConsList(object):
             return False
         
     def __repr__(self):
-        return repr({'type': 'cons list', 'sequence': self.seq[self.start:]})
+        return repr({'type': 'cons list', 'sequence': self.getRest()})
             
