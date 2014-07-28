ea5ce383cda62e905529da89372fd8f7feabe340
Matt Fenwick <mfenwick100@gmail.com>
2014-07-28 08:38:41 -0400

    include solution to pypi problems in build script

--- build.sh
+++ build.sh
@@ -1,1 +1,8 @@
 python setup.py sdist upload
+# having any problems getting this to run due to authentication?
+# check out:
+#   http://stackoverflow.com/questions/3773613/pypi-issues-upload-failed-401-you-must-be-identified-to-edit-package-inform
+# need a file ~/.pypirc with the following section:
+#   [server-login]
+#   username:mfenwick
+#   password:<the_password>
